5404e0bd58595c911d67199059fb0f9a3441799e
drovocek <mycodepg@gmail.com>
2022-11-06 10:06:31 +0300

    add ldap

--- httpBasic/src/main/java/ru/soft/config/UserManagementConfig.java
+++ httpBasic/src/main/java/ru/soft/config/UserManagementConfig.java
@@ -5,28 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import javax.sql.DataSource;
+import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 
 @Configuration
 public class UserManagementConfig {
 
     @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        String usersByUsernameQuery =
-                "select username, password, enabled from spring.users where username = ?";
-        String authsByUserQuery =
-                "select username, authority from spring.authorities where username = ?";
-
-        var userDetailsManager = new
-                JdbcUserDetailsManager(dataSource);
-
-        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
-
-        return userDetailsManager;
+    public UserDetailsService userDetailsService() {
+        var cs = new DefaultSpringSecurityContextSource("ldap://127.0.0.1:33389/dc=springframework,dc=org");
+        cs.afterPropertiesSet();
+        var manager = new LdapUserDetailsManager(cs);
+        manager.setUsernameMapper(new DefaultLdapUsernameToDnMapper("ou=groups", "uid"));
+        manager.setGroupSearchBase("ou=groups");
+        return manager;
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
